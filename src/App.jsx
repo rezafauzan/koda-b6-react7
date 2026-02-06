58f766d3d462650d2c624886261851600a1db7f8
rezafauzan <rezafauzan593@gmail.com>
2026-02-06 14:04:32 +0700

    Feat: add feature when checkbox clicked todo name will be line-through; add moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,75 @@
+import React from "react"
+import moment from "moment"
+
+const todo = [
+    {
+        id: 1,
+        name: "cuci pakaian",
+        createdAt: "22 januari 2025",
+        doneAt: "24 januari 2025",
+        target: "26 januari 2025",
+    },
+    {
+        id: 2,
+        name: "beli bahan makanan seminggu",
+        createdAt: "22 januari 2025",
+        doneAt: "24 januari 2025",
+        target: "26 januari 2025",
+    },
+    {
+        id: 3,
+        name: "cuci motor",
+        createdAt: "22 januari 2025",
+        doneAt: "24 januari 2025",
+        target: "26 januari 2025",
+    },
+    {
+        id: 4,
+        name: "weekly",
+        createdAt: "22 januari 2025",
+        doneAt: "24 januari 2025",
+        target: "26 januari 2025",
+    }
+]
+
+const TodoItem = () => {
+    const checkbox = React.useRef()
+    const todoName = React.useRef()
+    const todoDone = React.useRef()
+    function toggleStatus() {
+        if (checkbox.current.checked) {
+            todoName.current.classList.add('line-through')
+            todoDone.current.textContent = moment().format('DD-MM-YYYY hh:mm:ss')
+        } else {
+            setStatus(false)
+            todoName.current.classList.remove('line-through')
+            todoDone.current.textContent = ''
+        }
+    }
+    return (
+        <label>
+            <div className="flex gap-4 flex-1">
+                <input ref={checkbox} type="checkbox" id="checkbox" name="todo" className="w-7 h-7" onChange={toggleStatus} />
+                <span ref={todoName}>Tugas 1</span>
+            </div>
+            <div className="flex flex-col pl-16 flex-1">
+                <div className="flex">
+                    <span className="font-bold w-50">Tanggal dibuat:</span>
+                    {/* <span ref={todoCreated} className="flex"> 28 Januari 2026</span> */}
+                </div>
+                <div className="flex">
+                    <span className="font-bold w-50">Target selesai:</span>
+                    <span ref={todoDeadline} className="flex">02 Februari 2026</span>
+                </div>
+                <div className="flex">
+                    <span className="font-bold w-50">Selesai pada:</span>
+                    <span ref={todoDone} className="flex">01 Februari 2026</span>
+                </div>
+            </div>
+        </label>
+    )
+}
+
 function App() {
     return (
         <section>
@@ -9,12 +81,7 @@ function App() {
                     <h1 className="w-full text-xl font-bold">To Do:</h1>
                     <ul className="w-full flex flex-col gap-4">
                         <li>
-                            <label className="flex gap-4 w-full">
-                                <input type="checkbox" id="checkbox" name="todo" className="w-7 h-7"/>
-                                <span>Tugas 1</span>
-                                <span><span className="font-bold">Tanggal dibuat:</span> 28 Januari 2026</span>
-                                <span><span className="font-bold">Target selesai:</span> 02 Februari 2026</span>
-                            </label>
+                            <TodoItem />
                         </li>
                     </ul>
                 </div>
